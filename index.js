48280e33a4af87e15479a796098fa8295e27d651
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-04-01 20:14:06 -0400

    finished lab

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(" ")
+    let titleCaseWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    return titleCaseWords.join(" ")
+  })
 }
